7babd70ed37667b62d4c76a7aa043b25a35f33ef
Marko Mekjavic <marko.mekjavic@epfl.ch>
2024-03-18 15:08:09 +0000

    adding the installation instructions in a form of scripts

---
+++ install_conda_env.sh
@@ -0,0 +1,15 @@
+# Download and install Miniforge (an equivalent of Miniconda)
+wget "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh" -O ~/miniforge.sh
+bash ~/miniforge.sh -b -p ~/miniforge3
+
+# Delete installer
+rm ~/miniforge.sh
+
+# Activate base env and run init for the future
+source ~/miniforge3/etc/profile.d/conda.sh
+
+# create the llava virtualenv
+conda ccreeate -n llava python=3.10 -y
+conda activate llava
+
+
---
+++ install_requirements.sh
@@ -0,0 +1,9 @@
+# create the llava virtualenv
+conda ccreeate -n llava python=3.10 -y
+conda activate llava
+pip install --upgrade pip  # enable PEP 660 support
+pip install -e .
+
+# install the training packages
+pip install -e ".[train]"
+pip install flash-attn --no-build-isolation
